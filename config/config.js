0e19d9244eaf3c7b35f524e78ea6e33cd2b9d6ab
Curtis Osano <osanocurt@hotmail.co.uk>
2017-02-06 13:10:12 +0000

    angular version of mcd

--- config/config.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  port: process.env.PORT || 8000,
-  db: 'mongodb://localhost/global'
-};
--- config/tokens.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  secret: process.env.SECRET || 'big shout out and mad love to WDI23'
-};
